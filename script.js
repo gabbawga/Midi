036ed091b0e2398e3a834c4efee80a7d26029e61
Gabriel Ferreira <gabrielferreiraa05@gmail.com>
2024-03-22 11:36:38 -0300

    alterando e comentado o Java Script

--- script.js
+++ script.js
@@ -1,46 +1,12 @@
-document.querySelector('tecla.pom')
+//Selecionando elemento do html para o javascript e estamos atribuindo ao atributo do elemento nossa função tocaSomPom
+document.querySelector('.tecla_pom').onclick = tocaSomPom
 
 
+// Desta forma temos uma função que irá executar o bloco de código abaixo.
+function tocaSomPom(){
 
+    // Após a função ser chamada irá executar: play de uma tag de multimidia da pagina html.
+    document.querySelector('#som_tecla_pom').play();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
